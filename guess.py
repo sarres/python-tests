f73b1d8af6eddd039d73f68bd43f12e672b0e88f
Fernando Sarres <sarresfernando@gmail.com>
2011-09-25 21:13:54 -0300

    Primeira versão do programa de adivinhação

---
+++ guess.py
@@ -0,0 +1,8 @@
+print("Olah")
+g = input("Adivinhe o numero que eu estou pensando: ")
+guess = int(g)
+if guess == 5:
+    print("Você venceu , parabens!!")
+else:
+    print("Você perdeu :(")
+print ("acabou o jogo")
